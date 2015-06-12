2842842c465fd1b94df1faf51610858573b3d86d
Ben Margolis <ben833@gmail.com>
2015-06-12 13:52:32 -0400

    Adding some initial code and not allowing config.json in the repo

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+var slackbot = require('node-slackbot');
+
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+var bot = new slackbot('token_goes_here');
+
 require("./do_vote.js");
 require("./list_votes.js");
+
