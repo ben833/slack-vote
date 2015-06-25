1da9747acc2a3f77c9a23548891d97c5b1bcb3e9
Kevin Barnett <kevinbarnett5506@yahoo.com>
2015-06-25 13:17:22 -0700

    added start route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express')
 	, router = module.exports = express.Router()
   , doVote = require('./do_vote.js')
-  , listVotes = require('./list_votes.js');
+  , listVotes = require('./list_votes.js'),
+    startVotes = require('./start_vote.js');
 
 router.route('/vote').post(doVote.post);
 router.route('/votes').post(listVotes.post);
-router.route('/start').post();
+router.route('/start').post(startVotes.post);
