425921d1e260c3fee71e6f2002039e3af720dc70
Kevin Barnett <kevinbarnett5506@yahoo.com>
2015-06-25 12:33:21 -0700

    added sripts start

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(function (request, response) {
     response.status(200).send("Alive and well.")
 });
 
-var port = process.env.port || process.env.PORT || 3000;
+var port = process.env.port || process.env.PORT || 80;
 server.set('port',port);
 var hostname = process.env.hostname || "localhost";
 
