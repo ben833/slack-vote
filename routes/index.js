97d99c920d354440890b57a757459470c43ddc5e
Hans Gutknecht <hansdotcom@gmail.com>
2015-06-25 14:01:09 -0400

    Fix post/get issue

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
-	, router = module.exports = express.Router()
+  , router = module.exports = express.Router()
   , doVote = require('./do_vote.js')
   , listVotes = require('./list_votes.js');
 
 router.route('/vote').post(doVote.post);
-router.route('/votes').get(listVotes.get);
+router.route('/votes').post(listVotes.post);
