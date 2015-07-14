4d5b317f61de8bb1b1e5f966d54c887ce44bf76b
Hans Gutknecht <hansdotcom@gmail.com>
2015-07-14 14:46:19 -0400

    Working reds, added redis_to_go env vars

--- routes/start_vote.js
+++ routes/start_vote.js
@@ -1,8 +1,12 @@
 var data = require('./../data.js')
+  , redis = require('redis')
   , pollnameText = ''
   , triggerWord = ''
   , pollnameText = ''
-  , slackRes = '';
+  , slackRes = ''
+  , client = ''
+  , rtg = ''
+  , ts = Math.floor(Date.now() / 1000);
 
 exports.post = function (req, res, next) {
 
@@ -11,14 +15,26 @@ exports.post = function (req, res, next) {
   pollnameText = req.body.text;
   triggerWord = req.body.trigger_word;
   pollnameText = pollnameText.replace(triggerWord + ' ','');
-
-  console.log('start poll ' + pollnameText);
-
   data.pollName = pollnameText;
   data.votes = [];
   data.status = 1;
+  console.log('start poll ' + pollnameText);
 
-  console.log('Data object:', data)
+  /*
+   * Persist new poll to reddis
+   */
+  if (process.env.REDISTOGO_URL) {
+    rtg = require('url').parse(process.env.REDISTOGO_URL);
+    client = redis.createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(':')[1]);
+  } else {
+    client = redis.createClient();
+  }
+  client.on('connect', function() {
+    console.log('connected to reddis');
+    client.set('key' + ts, JSON.stringify(data));
+    console.log('saved new poll data');
+  });
 
   slackRes = data.pollName + '\n' + "enter vote _choice_ to submit your vote";
   res.json({text: slackRes});
