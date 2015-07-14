571fb43a1a367b007d60a58295243bc708618112
Hans Gutknecht <hansdotcom@gmail.com>
2015-07-14 14:27:45 -0400

    Code formatting

--- routes/start_vote.js
+++ routes/start_vote.js
@@ -1,19 +1,26 @@
-var data = require('./../data.js');
+var data = require('./../data.js')
+  , pollnameText = ''
+  , triggerWord = ''
+  , pollnameText = ''
+  , slackRes = '';
 
 exports.post = function (req, res, next) {
-    console.log('List route.');
-    pollnameText = req.body.text;
-    triggerWord = req.body.trigger_word;
-    pollnameText = pollnameText.replace(triggerWord + ' ','');
 
-    console.log('start poll ' + pollnameText);
+  console.log('List route.');
 
-    data.pollName = pollnameText;
-    data.votes = [];
-    data.status = 1;
+  pollnameText = req.body.text;
+  triggerWord = req.body.trigger_word;
+  pollnameText = pollnameText.replace(triggerWord + ' ','');
 
-    console.log("data object:", data)
+  console.log('start poll ' + pollnameText);
 
-    slackRes = data.pollName + '\n' + "enter vote _choice_ to submit your vote";
-    res.json({text: slackRes});
+  data.pollName = pollnameText;
+  data.votes = [];
+  data.status = 1;
+
+  console.log('Data object:', data)
+
+  slackRes = data.pollName + '\n' + "enter vote _choice_ to submit your vote";
+  res.json({text: slackRes});
+
 };
